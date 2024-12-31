6812f2190c6b96090a4025dfde8246fdd64f12bb
coderkhayrul <coderkhayrul@gmail.com>
2025-01-01 01:47:39 +0600

    Program-4

---
+++ program-4.py
@@ -0,0 +1,14 @@
+
+number1 = 33
+number2 = 33
+string = "Kahyrul"
+float = 33.33
+
+
+
+# print(type(float))
+print(f"{number1} + {number2} = {number1 + number2}", end=" ")
+
+
+print(f"{string} + {string} = {string + string}")
+
