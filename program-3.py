a33adccb33b62935893ad441060f44bd558d6a9a
coderkhayrul <coderkhayrul@gmail.com>
2025-01-01 01:37:27 +0600

    Program-3

---
+++ program-3.py
@@ -0,0 +1,18 @@
+from math import *
+
+
+print(sqrt(25))
+
+print(max(1, 2, 3, 4, 5))
+
+print(min(1, 2, 3, 4, 5))
+
+print(abs(-10))
+
+print(pow(2, 3))
+
+print(round(3.141592653589793))
+
+print(floor(3.141592653589793))
+
+print(ceil(3.141592653589793))
